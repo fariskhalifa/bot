5d5024e8cb586836562838970374bf6dad2c7cfd
fariskhalifa <103905775+fariskhalifa@users.noreply.github.com>
2023-02-17 17:06:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "yusril";
 const donet = "https://saweria.co/sansekai";
-const owner = ["212 714-835938"];
+const owner = ["212 714835938"];
 const {
   default: sansekaiConnect,
   useSingleFileAuthState,
